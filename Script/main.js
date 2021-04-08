7faa021e7130e5976f6d5c96c550e4cda0b0b547
Timothy Exodus <exodustimothy@gmail.com>
2021-04-08 16:54:55 +0100

    reponsive v4

---
+++ Script/main.js
@@ -0,0 +1,8 @@
+// const register_bg = document.querySelector(".register_bg");
+// const register = document.querySelector(".register");
+
+// function register_width(event) {
+//   register_bg.target.style.width = "50px";
+// }
+
+// register.addEventListener("mouseover", register_width);
